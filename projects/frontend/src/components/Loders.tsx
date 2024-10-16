011acaea08eb99636e0ac7f0041d4ff38f008bb1
HarshilMinddeft <harshil.mandali@minddeft.net>
2024-10-16 18:22:31 +0530

    DropdownStreamrateFuncitons,validations

--- projects/frontend/src/components/Loders.tsx
+++ projects/frontend/src/components/Loders.tsx
@@ -2,6 +2,6 @@ import { BlinkBlur } from 'react-loading-indicators'
 import OrbitProgress from 'react-loading-indicators/OrbitProgress'
 
 const LoadingOrbit = () => <OrbitProgress variant="track-disc" color="crimson" size="small" />
-const BlinkBlurB = () => <BlinkBlur color="00ce00"></BlinkBlur>
+const BlinkBlurB = () => <BlinkBlur speedPlus={2} size="small" color="00ce00"></BlinkBlur>
 
 export default BlinkBlurB
